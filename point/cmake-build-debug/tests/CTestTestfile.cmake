9909b44d0c2c2ecaf0853dbe5fb1fe1d2184a962
Stamaks <A6437149@yandex.ru>
2018-10-06 18:22:52 +0300

    Deleted old

--- point/cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stamaks/CLionProjects/Inst/point/tests
-# Build directory: /home/stamaks/CLionProjects/Inst/point/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest-build")
