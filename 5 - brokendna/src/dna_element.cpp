875d4adfc06aadb4e852112223896c8845180ae5
Stamaks <A6437149@yandex.ru>
2018-10-14 21:09:21 +0300

    Done dna_element

--- 5 - brokendna/src/dna_element.cpp
+++ 5 - brokendna/src/dna_element.cpp
@@ -13,4 +13,35 @@
 
 #include "dna_element.h"
 
-// TODO: implement
+DNAElement::DNAElement()
+{
+    id = ' ';
+    number = -1;
+    base = "";
+}
+
+DNAElement::DNAElement(const std::string &description)
+{
+    readFromString(description);
+}
+
+void DNAElement::readFromString(const std::string &description)
+{
+    if (isdigit(description[0]))
+        throw std::invalid_argument("Wrong arg in pos 0");
+
+    if (!isdigit(description[1]))
+        throw std::invalid_argument("Wrong arg in pos 1");
+
+    if (description[2] != ':')
+        throw std::invalid_argument("Wrong arg in pos 2");
+
+    if (description[3] != 'A' && description[3] != 'C' && description[3] != 'G' && description[3] != 'T')
+        throw std::invalid_argument("Wrong arg in pos 3");
+
+    id = description[0];
+    number = description[1] - '0'; // Дамми-способ перевести char в int
+    base = description[3];
+}
+
+
