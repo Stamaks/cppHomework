a355a57e6eb9ed2a09194d06c4511f3f4c248c00
Stamaks <A6437149@yandex.ru>
2018-11-16 19:15:35 +0300

    Started algo

--- 8 - europebyrail/src/rail_system.cpp
+++ 8 - europebyrail/src/rail_system.cpp
@@ -6,6 +6,30 @@
 #include <iostream>
 #include <fstream>
 
+//TODO: REWRITE
+RailSystem::RailSystem(const std::string &filename)
+{
+//    load_services(filename);
+
+}
+
+RailSystem::~RailSystem()
+{
+    for (std::pair<std::string, City*> pair : cities)
+    {
+        delete pair.second;
+    }
+
+    for (std::pair<std::string, std::list<Service*>> pair : outgoing_services)
+    {
+        for (Service* service : pair.second)
+        {
+            delete service;
+        }
+    }
+}
+
+
 void RailSystem::load_services(const std::string &filename)
 {
     std::string line;
@@ -91,14 +115,57 @@ void RailSystem::load_services(const std::string &filename)
 
 }
 
-//TODO: REWRITE
-RailSystem::RailSystem(const std::string &filename)
+void RailSystem::reset()
 {
-//    load_services(filename);
-
+    for (std::pair<std::string, City*> pair : cities)
+    {
+        pair.second->from_city = "";
+        pair.second->total_distance = 0;
+        pair.second->total_fee = 0;
+        pair.second->visited = 0;
+    }
 }
 
-//TODO: REWRITE
-RailSystem::~RailSystem()
+std::pair<int, int> RailSystem::calc_route(std::string from, std::string to)
 {
+    // Создаем вектор дистанций и инициализируем его максимальным значением
+    std::map<std::string, int> current_distances;
+    std::map<std::string, int> current_fees;
+    std::map<std::string, bool> used;
+
+    // Сообщаем мапам, какие у нас есть вершины
+    for (std::pair<std::string, City*> pair : cities)
+    {
+        current_distances[pair.first] = INT_MAX;
+        current_fees[pair.first] = 0;
+        used[pair.first] = false;
+    }
+
+    for (int i = 0; i < outgoing_services.size(); ++i) {
+        int v = -1;
+
+        for (std::pair<std::string, std::list<Service*>> pair : outgoing_services)
+        {
+            if (!used[pair.first] && (v == -1 || current_distances[pair.first] < current_distances[pair.first]))
+                v = j;
+        }
+
+        if (current_distances[v] == INT_MAX)
+            break;
+
+        used[v] = true;
+
+        for (size_t j=0; j < g[v].size(); ++j) {
+            int to = g[v][j].first,
+                    len = g[v][j].second;
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+            }
+        }
+    }
+
+
+
+
 }
